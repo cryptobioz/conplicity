4c8d16dc8b40db679dbde9909c56b1d491de38ae
Léo Depriester <leo.depriester@exadot.fr>
2018-09-22 22:50:56 +0200

    Start from scratch

--- bivac_test.go
+++ bivac_test.go
@@ -1,10 +1,27 @@
 package main
 
-import (
-	"os"
-	"testing"
-)
-
-func TestMain(m *testing.M) {
-	os.Exit(m.Run())
-}
+//import (
+//	"testing"
+//)
+//
+//func TestTeardownThreads(t *testing.T) {
+//	expectedResult := 1
+//
+//	volumesManagerStop := make(chan struct{})
+//	volumesManagerStopped := make(chan struct{})
+//
+//	go func() {
+//		defer close(volumesManagerStopped)
+//
+//		select {
+//		case <-volumesManagerStop:
+//			return
+//		}
+//	}()
+//
+//	result := teardownThreads(1, volumesManagerStop, volumesManagerStopped)
+//
+//	if result != expectedResult {
+//		t.Fatalf("Expected %d, got %d", expectedResult, result)
+//	}
+//}
